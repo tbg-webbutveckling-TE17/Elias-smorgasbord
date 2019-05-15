487feca6e9027265c7c7adb865b1cf72545c8ef8
Ribban <doppel2ganger@gmail.com>
2019-05-15 13:14:40 +0200

    Idag fortsatte jag med att fixa mina sidebars. Ena är nu vackert synkroniserad med sidans scrollfunktion dock så påverkas mitten-elementet och den andra sidebaren. Försökte göra ett till element i javascript till den andra sidebaren men till inget resultat.

--- Projects/Slutprojekt/assets/js.js
+++ Projects/Slutprojekt/assets/js.js
@@ -1,13 +1,29 @@
 window.onscroll = function() {myFunction()};
 
-var nav = document.getElementById("mybar");
+var nav = document.getElementById("leftbar");
 
-var sticky = nav.offsetTop;
+var leftsticky = nav.offsetTop;
 
 function myFunction() {
-  if (window.pageYOffset > 500) {
-    nav.classList.add("sticky");
-  } else {
-    nav.classList.remove("sticky");
+	if (window.pageYOffset > 550) {
+		nav.classList.add("leftsticky");
+  } 
+	else {
+		nav.classList.remove("leftsticky");
   }
+}
+
+window.onscroll =function() {myFunction()}
+
+var navs = document.getElementById("rightbar");
+
+var rightsticky = navs.offsetTop;
+
+function myfunction() {
+	if (window.pageYOffset > 550) {
+		navs.classList.add("rightsticky");
+	}
+	else {
+		navs.classList.remove("rightsticky");
+	}
 }
