0e8e77e3efd871dfaa515b120b7096db70a77794
Ribban <doppel2ganger@gmail.com>
2019-05-17 11:40:49 +0200

    Tack vare dennis fungerar nu även den andra sidebaren. Enda problemet är mittenelementet som påverkas när man scrollar.

--- Projects/Slutprojekt/assets/js.js
+++ Projects/Slutprojekt/assets/js.js
@@ -1,29 +1,22 @@
 window.onscroll = function() {myFunction()};
 
 var nav = document.getElementById("leftbar");
+var navs = document.getElementById("rightbar");
+var navss = document.getElementById("mainbar")
 
 var leftsticky = nav.offsetTop;
+var rightsticky = navs.offsetTop;
+var mainsticky = navss.offsetTop;
 
 function myFunction() {
 	if (window.pageYOffset > 550) {
 		nav.classList.add("leftsticky");
+		navs.classList.add("rightsticky");
+		navss.classList.add("mainsticky")
   } 
 	else {
 		nav.classList.remove("leftsticky");
-  }
-}
-
-window.onscroll =function() {myFunction()}
-
-var navs = document.getElementById("rightbar");
-
-var rightsticky = navs.offsetTop;
-
-function myfunction() {
-	if (window.pageYOffset > 550) {
-		navs.classList.add("rightsticky");
-	}
-	else {
 		navs.classList.remove("rightsticky");
-	}
+		navss.classList.remove("mainsticky")
+  }
 }
