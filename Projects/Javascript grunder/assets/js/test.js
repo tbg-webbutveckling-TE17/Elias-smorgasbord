e053d1c89624a7129850ec8ff3767352c80428e3
Ribban <doppel2ganger@gmail.com>
2019-09-20 11:21:51 +0200

    Har följt Dennis när han förklarat javascript

--- Projects/Javascript grunder/assets/js/test.js
+++ Projects/Javascript grunder/assets/js/test.js
@@ -1,1 +1,62 @@
-alert('Are we connected?');
+// alert('Popup ruta');
+
+var myName = "Elias"; // datatyp string
+var myAge = "18"; // datatyp Number
+var isEvil = false; // datatyp Boolean
+
+// Single line content
+/*
+    Block content
+*/
+
+/**
+ 
+    -------------------------
+            If/else
+    -------------------------
+
+ */
+
+if(myAge < 18) {
+    //alert("You can not take a drivers licens.");
+} else {
+    //alert("Congratz! You are old enough to drive.");
+};
+
+if(myName == "Elias") {
+    //alert("Welcome, " + myName);
+} else {
+    //alert("You are not welcome here!");
+}
+
+// == equals to, === equals to and same datatype
+if(myAge == "18") {
+    //alert("Välkommen");
+} else {
+    //alert("Icket välkommen");
+}
+
+var hasWatched = false; //add to watchlist
+
+if(isEvil == true) {
+    //alert("Du är ond");
+} else {
+    //alert("Du är god");
+}
+
+/* 
+
+    *********************
+        Guessing app
+    *********************
+
+*/
+
+var secretNumber = 7;
+var userInput = Number(prompt("Guess a number between 0 and 10"));
+
+if(userInput === secretNumber) {
+    document.writeln("Grattis du har gissat rätt! <br>");
+} elseif(userInput === 6 ) {
+    document.writeln("Det bränns");
+}
