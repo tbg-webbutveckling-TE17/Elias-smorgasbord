27ed0986b9e8907cfba9141b226c49459db0abbf
Ribban <doppel2ganger@gmail.com>
2019-10-02 12:39:55 +0200

    Gjorde for loopar. Förväntar mig ett A

--- Projects/Javascript grunder/assets/js/3 - for.js
+++ Projects/Javascript grunder/assets/js/3 - for.js
@@ -1,18 +1,37 @@
 //For använder man när man vet hur många gånger loopen ska köra
 
 //initiering; villkor; inkrementering
-for(var i = 0; i < 6; i++) {
+//for(var i = 0; i < 6; i++) {
     // kör koden för varje iteration
+    //console.log(i);
+
+
+//var str = "Hello World!";
+//for(var i = 0; i < str.length - 1; i++) {
+    //console.log(str[i]);
+
+
+//var myArr = [ "HTML", "CSS", "JS", 42, true ];
+//var len = myArr.length;
+//for (var i = 0; i < len; i++) {
+    //console.log(myArr[i]);
+
+for(var i = -10; i <= 19; i++) {
     console.log(i);
 }
 
-var str = "Hello World!";
-for(var i = 0; i < str.length - 1; i++) {
-    console.log(str[i]);
+for(var i = 10; i <= 40; i+= 2) {
+    console.log(i);
 }
 
-var myArr = [ "HTML", "CSS", "JS", 42, true ];
-var len = myArr.length;
-for (var i = 0; i < len; i++) {
-    console.log(myArr[i]);
+for(var i = 300; i <= 333; i++) {
+    if(i % 2 !== 0) {
+        console.log(i);
+    }
+}
+
+for(var i = 5; i <=50; i++) {
+    if(i % 5 === 0 && i % 3 === 0) {
+        console.log(i);
+    }
 }
