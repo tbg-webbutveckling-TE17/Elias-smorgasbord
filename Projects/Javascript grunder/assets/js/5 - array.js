5e13004003f1adb4940523c2d010c3f8eb6274a1
Ribban <doppel2ganger@gmail.com>
2020-01-14 13:57:35 +0100

    Har fortsatt med arrays

--- Projects/Javascript grunder/assets/js/5 - array.js
+++ Projects/Javascript grunder/assets/js/5 - array.js
@@ -26,3 +26,43 @@ printToScreen.innerHTML = colors.join(' <br /> ');
 
 //pop() - remove an item at the end of an array
 colors.pop();
+printToScreen.innerHTML = colors.join(' <br /> ');
+
+// push() - add an item at the end of an array
+colors.push("purple");
+printToScreen.innerHTML = colors.join(' <br /> ');
+
+// unshift() - add an item at the beginning of an array
+colors.unshift("yellow");
+printToScreen.innerHTML = colors.join(' <br /> ');
+
+// shift() - remove an item at the beginning of an array
+colors.shift();
+printToScreen.innerHTML = colors.join(' <br /> ');
+
+// slice() - copy parts of an array
+var colorGreen = colors.slice(1,2);
+printToScreen.innerHTML = colorGreen.join(' ');
+
+var colorsCopy = colors.slice(colors);
+printToScreen.innerHTML = colorsCopy.join(' ');
+
+// indexOf - find the index of an item in the array
+printToScreen.innerHTML = colors.indexOf("green");
+printToScreen.innerHTML = colors.indexOf("yellow");
+
+
+
+var numbers = [ 2, 4, 30, 31 ];
+printToScreen.innerHTML = numbers[numbers.length];
+
+var cars = [ 'Audi', 'Volvo', 'Ford', 'Ferarri', 'Landa', 'Lada', 'Citroën' ];
+for(var i = 0; i< cars.length; i++) {
+    printToScreen.innerHTML += cars[i] * '<br />';
+}
+printToScreen.innerHTML = cars.reverse;
+
+var countries = ["Sweden", "Norway", "Finland", "Denmark", "Island"];
+countries.forEach(function(country) {
+    printToScreen.innerHTML += country + " ";
+})
