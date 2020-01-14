7b8a69951317d3396849a901d2f1a1aa3ae5ec57
Ribban <doppel2ganger@gmail.com>
2020-01-14 12:23:41 +0100

    Har jobbat med Hangman men ej fått det att funka

--- Projects/Javascript grunder/assets/js/hangman.js
+++ Projects/Javascript grunder/assets/js/hangman.js
@@ -1,10 +1,40 @@
 var guesses = 10;
+var wins = 0;
+var losses = 0;
 var word = ["G", "O", "A", "T"];
 var hiddenWord = ["_", "_", "_", "_"];
+var newHiddenWord = ["_", "_", "_", "_"];
+var displayHiddenWord = document.getElementsByTagName("h3") [0]
+
+var showGuesses = document.getElementsByClassName("guesses") [0];
+var showWins = document.getElementsByClassName("wins") [0];
+var showLosses = document.getElementsByClassName("losses") [0];
+displayHiddenWord.innerHTML = hiddenWord;
+
+showGuesses.innerHTML = "You have " + guesses + " left";
+showWins.innerHTML = "You have " + wins + " wins";
+showLosses.innerHTML = "You have " + losses + " losses";
 
 document.addEventListener("keypress", function(e) {
-    var guess = e.key
-    console.log(guess)
-
-    document.getElementsByClassName("show").innerHTML += guess;
+    var guess = e.key;
+    document.getElementsByClassName("show").innerHTML = hiddenWord;
+    for (var i = 0, i <= word.length; i++) {
+        if (guess == word[i]) {
+            hiddenWord[i] == guess;
+            displayHiddenWord.innerHTML = hiddenWord;
+        }
+    }
+    if (hiddenWord == word) {
+        
+    }
+    if (guesses == 0) {
+        losses++;
+        showLosses.innerHTML = "You have " + losses + " losses";
+        guesses = 10;
+        hiddenWord = newHiddenWord;
+        showGuesses.innerHTML = "You have " + guesses + " guesses left"
+    } else {
+        guesses--;
+        showGuesses.innerHTML = "You have " + guesses + " guesses left"
+    }
 })
