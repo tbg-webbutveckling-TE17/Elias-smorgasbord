b91939ecf82d058f7f6ff99a2933355c4fde782c
Ribban <doppel2ganger@gmail.com>
2020-04-07 13:59:32 +0200

    push nr 35

--- Projects/Datorsida/assets/css/js.js
+++ Projects/Datorsida/assets/css/js.js
@@ -1,14 +1,14 @@
-var collapse = document.getElementsByClassName("button1");
+var collapse = document.getElementsByClassName("button");
 var i;
 
 for (i = 0; i < collapse.length; i++) {
     collapse[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        var content = this.nextSibling;
+        var content = this.nextElementSibling;
         if (content.style.display === "block") {
             content.style.display = "none";
         } else {
-            content.style.display = "block;"
+            content.style.display = "block";
         }
     });
 }
