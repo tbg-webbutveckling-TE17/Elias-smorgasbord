0dbec580de1f74e5ce60febb99ec1681c894570c
Ribban <44964120+Ribban@users.noreply.github.com>
2020-06-01 11:44:07 +0200

    fortsatt med grejer

--- Projects/Datorsida/assets/css/js.js
+++ Projects/Datorsida/assets/css/js.js
@@ -21,18 +21,45 @@ function addToCartClick() {
     var title = part.getElementsByClassName(h2)[0];
     var itemPrice = part.getElementsByClassName("price")[0];
     var imgSrc = part.getElementsByClassName(img)[0];
+    addItemToCart(title, itemPrice, imgSrc)
+    updateCart();
 }
 
 function addItemToCart(title, itemPrice, imgSrc) {
-
+    var cartRow = document.createElement("section");
+    cartRow.classList.add("cart-row");
+    var cartItems = document.getElementsByClassName("cart-items")[0];
+    var cartItemNames = cartItems.getElementsByClassName("cart-item-title");
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert("Den här produkten är redan i din kundvagn");
+            return;
+        }
+    }
+    var cartRowContents =`
+        <section class="cart-item cart-column">
+            <img class="cart-item-image" src="${imgSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </section>
+        <span class="cart-price cart-column">${itemPrice}</span>
+        <section class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn-danger" type="button">REMOVE</button>
+        </section>`
+    cartRow.innerHTML = cartRowContents;
+    cartItems.append(cartRow);
+    cartRow.getElementsByClassName("btn-danger")[0].addEventListener("click", remove);
+    cartRow.getElementsByClassName("cart-quantity-input")[0].addEventListener("change", quantity);
 }
 
-function quantity() {
-
+function quantity(event) {
+    var input = event.target;
 }
 
-function remove() {
-
+function remove(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateCart();
 }
 
 function purchase() {
